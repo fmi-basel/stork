4c0b63286cff7206cb955b890d99f99025c09949
Julia <julia.gygax@gmail.com>
2022-11-15 14:39:41 +0100

    Added several versions of stochastic spike activation functions

--- stork/activations.py
+++ stork/activations.py
@@ -226,7 +226,7 @@ class SigmoidSpike(torch.autograd.Function):
 
     The steepness parameter beta can be accessed via the static member self.beta (default=100).
     """
-    beta = 100.0
+    beta = 2
 
     @staticmethod
     def forward(ctx, input):
@@ -254,6 +254,180 @@ class SigmoidSpike(torch.autograd.Function):
         sig = torch.sigmoid(SigmoidSpike.beta*input)
         dsig = sig*(1.0-sig)
         grad = grad_input*dsig
+        return grad
+
+
+class StochasticSpike(torch.autograd.Function):
+    """
+    Stochastic spike implementation, where the probability of a spike follows a sigmoid. The backward path uses a straight-through estimator, where the derivative of a hard threshold is 1 and the derivative of the probability of spiking (derivative of a sigmoid) is consiedered in the backward pass.
+
+    The steepness parameter beta can be accessed via the static member self.beta (default=20).
+    """
+    beta = 2
+
+    @staticmethod
+    def forward(ctx, input):
+        """
+        In the forward pass we receive a Tensor containing the input and return
+        a Tensor containing the step function output. ctx is the context object
+        that is used to stash information for backward pass computations. 
+        """
+        ctx.save_for_backward(input)
+        out = torch.zeros_like(input)
+        prob = torch.sigmoid(StochasticSpike.beta*input)
+        if prob.get_device() < 0:
+            p = torch.rand(size=prob.shape)
+        else:
+            p = torch.rand(size=prob.shape, device=prob.get_device())
+        out[prob > p] = 1
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor containing the gradient of the
+        loss with respect to the output, and we compute the surrogate gradient
+        of the loss with respect to the input. Here we assume the standardized
+        negative part of a fast sigmoid as this was done in Zenke & Ganguli
+        (2018).
+        """
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        sig = torch.sigmoid(StochasticSpike.beta*input)
+        dsig = sig*(1.0-sig)
+        grad = grad_input*dsig
+        return grad
+
+
+class ExponentialStochasticSpike(torch.autograd.Function):
+    """
+    Stochastic spike implementation, where the probability of a spike follows an exponential. The backward pass uses the derivative of the probability of spiking.
+
+    The parameters p0 and delta_u can be accessed via the static member self.p0 (default=0.01) or self.delta_u (default=0.2).
+    """
+    p0 = 0.01
+    delta_u = 0.013
+    eps_0 = 0.267
+
+    @staticmethod
+    def forward(ctx, input):
+        """
+        In the forward pass we receive a Tensor containing the input and return
+        a Tensor containing the step function output. ctx is the context object
+        that is used to stash information for backward pass computations. 
+        """
+        ctx.save_for_backward(input)
+        out = torch.zeros_like(input)
+        prob = ExponentialStochasticSpike.p0 * \
+            torch.exp(input*ExponentialStochasticSpike.eps_0 /
+                      ExponentialStochasticSpike.delta_u)
+        if prob.get_device() < 0:
+            p = torch.rand(size=prob.shape)
+        else:
+            p = torch.rand(size=prob.shape, device=prob.get_device())
+        out[prob > p] = 1
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor containing the gradient of the
+        loss with respect to the output, and we compute the surrogate gradient
+        of the loss with respect to the input. Here we assume the standardized
+        negative part of a fast sigmoid as this was done in Zenke & Ganguli
+        (2018).
+        """
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        p = ExponentialStochasticSpike.p0 * \
+            torch.exp(input*ExponentialStochasticSpike.eps_0 /
+                      ExponentialStochasticSpike.delta_u)
+        dp = p / ExponentialStochasticSpike.delta_u*ExponentialStochasticSpike.eps_0
+        grad = grad_input*dp
+        return grad
+
+
+class MultilayerSpikerSpike(torch.autograd.Function):
+    """
+    Stochastic spike implementation, where the probability of a spike follows an exponential. The backward pass uses the spiketrain itself as the derivative of the spike train.
+
+    The parameters p0 and delta_u can be accessed via the static member self.p0 (default=0.01) or self.delta_u (default=0.2).
+    """
+    p0 = 0.01
+    delta_u = 0.013
+    eps_0 = 0.267
+
+    @staticmethod
+    def forward(ctx, input):
+        """
+        In the forward pass we receive a Tensor containing the input and return
+        a Tensor containing the step function output. ctx is the context object
+        that is used to stash information for backward pass computations. 
+        """
+        out = torch.zeros_like(input)
+        prob = MultilayerSpikerSpike.p0 * \
+            torch.exp(input*MultilayerSpikerSpike.eps_0 /
+                      MultilayerSpikerSpike.delta_u)
+        if prob.get_device() < 0:
+            p = torch.rand(size=prob.shape)
+        else:
+            p = torch.rand(size=prob.shape, device=prob.get_device())
+        out[prob > p] = 1
+        ctx.save_for_backward(out)
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor containing the gradient of the
+        loss with respect to the output, and we compute the surrogate gradient
+        of the loss with respect to the input. Here we assume the standardized
+        negative part of a fast sigmoid as this was done in Zenke & Ganguli
+        (2018).
+        """
+        out, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad = grad_input*out
+        return grad
+
+
+class SigmoidalMultilayerSpikerSpike(torch.autograd.Function):
+    """
+    Stochastic spike implementation, where the probability of a spike follows a sigmoid. The backward pass uses the spiketrain itself as the derivative of the spike train.
+
+    The steepness parameter beta can be accessed via the static member self.beta (default=20).
+    """
+    beta = 2
+
+    @staticmethod
+    def forward(ctx, input):
+        """
+        In the forward pass we receive a Tensor containing the input and return
+        a Tensor containing the step function output. ctx is the context object
+        that is used to stash information for backward pass computations. 
+        """
+        ctx.save_for_backward(input)
+        out = torch.zeros_like(input)
+        prob = torch.sigmoid(SigmoidalMultilayerSpikerSpike.beta*input)
+        if prob.get_device() < 0:
+            p = torch.rand(size=prob.shape)
+        else:
+            p = torch.rand(size=prob.shape, device=prob.get_device())
+        out[prob > p] = 1
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor containing the gradient of the
+        loss with respect to the output, and we compute the surrogate gradient
+        of the loss with respect to the input. Here we assume the standardized
+        negative part of a fast sigmoid as this was done in Zenke & Ganguli
+        (2018).
+        """
+        out, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad = grad_input*out
         return grad
 
 
