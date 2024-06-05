72f34fc7add5f312122f1f6d46644d770d110bb9
Julia Gygax <julia.gygax@gmail.com>
2024-06-05 09:00:33 +0200

    two small changes for possible speed up

--- stork/nodes/lif/filterLIF.py
+++ stork/nodes/lif/filterLIF.py
@@ -80,11 +80,15 @@ class FilterLIFGroup(CellGroup):
         self.W_filt = None
         self.filt_param = None
         self.A = None
-        
+
         # Group membership tensor
-        assert self.nb_units % self.nb_groups == 0, "Nb_units must be divisible by nb_groups"
+        assert (
+            self.nb_units % self.nb_groups == 0
+        ), "Nb_units must be divisible by nb_groups"
         self.nb_units_per_group = self.nb_units // self.nb_groups
-        self.group_membership = torch.tensor(list(range(self.nb_groups)) * self.nb_units_per_group)
+        self.group_membership = torch.tensor(
+            list(range(self.nb_groups)) * self.nb_units_per_group
+        )
 
     def configure(self, batch_size, nb_steps, time_step, device, dtype):
         self.dcy_mem = float(np.exp(-time_step / self.tau_mem))
@@ -130,9 +134,19 @@ class FilterLIFGroup(CellGroup):
             *self.filter_shape, device=self.device, dtype=self.dtype
         )
 
-        self.W_filt = self.filt_param.expand(
-            size=(self.nb_units_per_group, *self.filt_param.shape)
-        ).reshape(shape=(self.nb_units, self.nb_filters)).to(self.device)
+        # self.W_filt = self.filt_param.repeat(self.nb_units_per_group, 1).to(self.device)
+
+        # self.W_filt = self.filt_param.unsqueeze(0).expand(
+        #     self.nb_units_per_group, self.nb_groups, self.nb_filters
+        # ).flatten(start_dim=0, end_dim=1)
+
+        self.W_filt = (
+            self.filt_param.expand(
+                size=(self.nb_units_per_group, *self.filt_param.shape)
+            )
+            .reshape(shape=(self.nb_units, self.nb_filters))
+            .to(self.device)
+        )
 
         self.mem = self.get_state_tensor("mem", state=self.mem)
         self.out = self.states["out"] = torch.zeros(
@@ -157,11 +171,10 @@ class FilterLIFGroup(CellGroup):
 
         # synaptic dynamics
         # Update filters (z=f)
-        new_filt = torch.einsum("bnf,fz->bnz", self.filt, self.A)
-        
+        new_filt = self.filt @ self.A
+
         # add spiketrain to first filters
-        new_filt[:, :, 0] += self.input
-        # TODO: check whether the input is added correctly
+        new_filt[:, :, 0].add_(self.input)
 
         syn_input = torch.einsum("nf,bnf->bn", self.W_filt, new_filt)
 
@@ -177,85 +190,88 @@ class FilterLIFGroup(CellGroup):
         self.out = self.states["out"] = new_out
         self.mem = self.states["mem"] = new_mem
         self.filt = new_filt
-        
-        
-    def get_filterbanks_by_group(self, sum_over_filters=True, T=None,  A=None, filt_param=None):
+
+    def get_filterbanks_by_group(
+        self, sum_over_filters=True, T=None, A=None, filt_param=None
+    ):
         """
         Used to inspect the learned synaptic filters.
         Returns an object of shape (time, nb_groups)
         that contains the weighted filterbanks for each group.
-        
+
         If sum_over_filters=False, returns an object of shape (time, nb_groups, nb_filters)
         with each individually weighted filter.
         """
-        
+
         if filt_param is None:
             filt_param = self.filt_param
-        
+
         if A is None:
             A = self.A
-            
+
         if T is None:
             T = self.nb_filters * self.tau_filter * 5
-        
+
         # Prepare empty tensors
         timesteps = int(T / self.time_step)
 
-        # Filter state 
+        # Filter state
         filter_shape = (self.nb_groups, self.nb_filters)
         filt_state = torch.zeros(filter_shape, device=self.device, dtype=self.dtype)
-        
+
         # Output
-        output = torch.zeros((timesteps + 1, *filter_shape), device=self.device, dtype=self.dtype)
-        
+        output = torch.zeros(
+            (timesteps + 1, *filter_shape), device=self.device, dtype=self.dtype
+        )
+
         # Evolve filters over time
         filt_state[..., 0] = 1
         output[0] = filt_state
 
         for timestep in range(timesteps):
-            
+
             # Update filters (z = f)
             filt_state = torch.einsum("gf,fz->gz", filt_state, A)
             output[timestep + 1] = filt_state
-        
+
         # Weight the filterbanks
-        w = filt_param.clone().detach() # shape [nb_groups, nb_filters]
-        output_weighted = (w.view(1, *w.shape) * output)
-        
+        w = filt_param.clone().detach()  # shape [nb_groups, nb_filters]
+        output_weighted = w.view(1, *w.shape) * output
+
         if sum_over_filters:
             return output_weighted.sum(-1).cpu().detach().numpy()
-        
+
         else:
             return output_weighted.cpu().detach().numpy()
-        
-        
+
     def get_membrane_kernel_by_group(self, time_step=None):
         """
         Used to inspect the learned PSP kernels.
         Returns an object of shape (time, nb_groups)
         that contains the post-synaptic potential kernel for each group.
-        
+
         I this is called before `model.configure` is called, it will draw random filter
         weights for each group.
         """
-        
-        device = torch.device('cpu')
+
+        device = torch.device("cpu")
         dtype = torch.float
-        
+
         if time_step == None:
             time_step = self.time_step
-        
+
         if self.filt_param == None:
             filt_param = torch.randn(
-                (self.nb_groups, self.nb_filters), device=device, dtype=dtype)
+                (self.nb_groups, self.nb_filters), device=device, dtype=dtype
+            )
         else:
             filt_param = self.filt_param.clone().detach().cpu()
 
         if self.A == None:
-            
+
             dcy_filter = float(np.exp(-time_step / self.tau_filter))
             scl_filter = 1 - self.dcy_filter
-            
+
             A_shape = (self.nb_filters, self.nb_filters)
             A = torch.zeros(A_shape, device=device, dtype=dtype)
             A.fill_diagonal_(dcy_filter)
@@ -266,32 +282,28 @@ class FilterLIFGroup(CellGroup):
 
         else:
             A = self.A.clone().detach().cpu()
-             
+
         # Compute filterbanks
         T = self.nb_filters * self.tau_filter * 5
-        
-        syn_currents = self.get_filterbanks_by_group(sum_over_filters=True,
-                                                    T=T, 
-                                                    A = A, 
-                                                    filt_param = filt_param)
-        
-        mem_by_group = lif_membrane_dynamics(syn_currents, 
-                                             self.tau_mem, 
-                                             dt=time_step)
-        
+
+        syn_currents = self.get_filterbanks_by_group(
+            sum_over_filters=True, T=T, A=A, filt_param=filt_param
+        )
+
+        mem_by_group = lif_membrane_dynamics(syn_currents, self.tau_mem, dt=time_step)
+
         return mem_by_group
-    
-    
+
     def get_epsilon_numerical(self, time_step):
         """
-        Returns numerical values of epsilon_bar and epsilon_hat 
+        Returns numerical values of epsilon_bar and epsilon_hat
         (used by fluctuation-driven initialization strategies)
         """
-        
+
         # shape [time, nb_groups]
         kernels = self.get_membrane_kernel_by_group(time_step)
-        
+
         epsilon_bar = (kernels.sum(0) * time_step).mean() + EPSILON
         epsilon_hat = ((kernels**2).sum(0) * time_step).mean()
-        
+
         return epsilon_bar, epsilon_hat
